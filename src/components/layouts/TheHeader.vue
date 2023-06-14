8f766e88a5e4e42ecb2f3d872c891d98a5355be8
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2023-06-14 11:38:30 +0700

    Initiate Project and dependencies

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="page-title-container">
+        <div class="row">
+            <!-- Title Start -->
+            <div class="col-12 col-sm-6">
+                <h1 class="mb-0 pb-0 display-4" id="title">
+                    {{ title }}
+                </h1>
+                <nav
+                    class="breadcrumb-container d-inline-block"
+                    aria-label="breadcrumb"
+                >
+                </nav>
+            </div>
+            <!-- Title End -->
+
+            <!-- Top Buttons Start -->
+            <div
+                class="col-12 col-sm-6 d-flex align-items-start justify-content-end"
+            >
+                <!-- Tour Button Start -->
+                <button
+                    type="button"
+                    class="btn btn-outline-primary btn-icon btn-icon-end w-100 w-sm-auto"
+                    id="dashboardTourButton"
+                >
+                    <span>Take a Tour</span>
+                    <i data-acorn-icon="flag"></i>
+                </button>
+                <!-- Tour Button End -->
+            </div>
+            <!-- Top Buttons End -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheHeader",
+    props: {
+        title: {
+            type: String,
+            default: "Please set title prop",
+        },
+    },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import store from '../store/index.js'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: { requiresAuth: true }
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+    meta: { requiresUnauth: true }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+  linkActiveClass: 'active'
+});
+
+router.beforeEach(function(to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/login');
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/');
+  } else {
+    next();
+  }
+});
+  
 
 export default router
